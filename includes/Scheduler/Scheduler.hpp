aead559033d4fc1714959f71897040115d1af864
reofard <6590f1@naver.com>
2024-12-05 04:27:31 +0000

    스케줄러에서 Task생성 로직 삭제

--- includes/Scheduler/Scheduler.hpp
+++ includes/Scheduler/Scheduler.hpp
@@ -1,29 +1,29 @@
 #include <memory>
 #include <string>
 #include <map>
-#include <set>
 #include <utility>
+#include <vector>
 
-class Robot;
+class MatchTable;
+
+class Executor;
 class Action;
-class Task;
 
-typedef std::pair<std::shared_ptr<Task>, std::shared_ptr<Robot>> MatchInfo;
+typedef int match_index;
 
 class Scheduler
 {
 private:
+    std::unique_ptr<MatchTable> match_table;            //로봇-Action 매칭 정보
 
-    std::map<int, MatchInfo> match_table;               // Task - Robot 매칭 정보
-    std::set<std::shared_ptr<Task>> tasks;              // 할당 Task 정보
     std::map<int, std::shared_ptr<Action>> actions;     // 수행 중 Action정보
 
 public:
 
-    // 입력받는 데이터 기반으로 Task, action 생성
-    void createTask();
+    // 스케줄링 할 Action 추가
+    void addAction(std::vector<std::shared_ptr<Action>> actions);
 
-    // 의존관계 생성, task 생성시 에만 사용하기때문에 private
-    void addActionDependency(std::shared_ptr<Action>& parent, std::shared_ptr<Action>& child);
+    // 다른 로봇이 수행해야하는 Action이 존재할 때 매칭 정보 생성 및 id 발급 함수
+    int addMatchInfo();
 
 };
--- src/Scheduler/Scheduler.cpp
+++ src/Scheduler/Scheduler.cpp
@@ -1,32 +1,17 @@
 #include "Scheduler/Scheduler.hpp"
+#include "Scheduler/MatchTable.hpp"
 #include "Scheduler/Action/Action.hpp"
-#include "Scheduler/Task/Task.hpp"
 
-
-// Task생성
-void Scheduler::createTask()
+void Scheduler::addAction(std::vector<std::shared_ptr<Action>> actions)
 {
-    std::shared_ptr<Task> task = std::make_shared<Task>("목업 테스크");
-
-    std::set<int> id_matcher;
-    // 지금은 목업데이터만 생성
-    for(int i = 0; i < 3; i++)
+    for(auto& action : actions)
     {
-        std::shared_ptr<Action> action = std::make_shared<Action>("", "", "");
         int id = action->getId();
-
-        id_matcher.insert(id);
-
-        task->addAction(action);
-        actions.insert({id, std::move(action)});
+        this->actions.insert({id, std::move(action)});
     }
-    // addActionDependency(id_matcher[0], id_matcher[2]);
-    // addActionDependency(id_matcher[1], id_matcher[2]);
 }
 
-// Task 내부의 Action간 의존관계 추가
-void Scheduler::addActionDependency(std::shared_ptr<Action>& parent, std::shared_ptr<Action>& child)
+int Scheduler::addMatchInfo()
 {
-    parent->addChild(child);
-    child->addParent(parent);
+    return match_table->issueMatchID();
 }
