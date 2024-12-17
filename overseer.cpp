d46df6146972c1d3e739f83beb12e29cf6b376fd
reofard <6590f1@gmail.com>
2024-12-17 08:57:09 +0000

    메인 로직에서 scheduler생성후 주기적으로 호출 이벤트 추가

--- overseer.cpp
+++ overseer.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include "Scheduler/Scheduler.hpp"
+#include "EventManager/EventManager.hpp"
+#include <thread>
+
+Scheduler* scheduler;
 
 int main()
 {
-  std::cout << "Hello, Overseer!" << std::endl;
-  return 0;
+    std::cout << "Hello, Overseer!" << std::endl;
+    scheduler = new Scheduler();
+
+    EventManager& instance = EventManager::getInstance();
+    instance.waitUntilStopped();
+
+    std::cout << "Bye Overseer!" << std::endl;
+
+    return 0;
 }
